19f993e0bf236828af3ce0569b91ebbaccfd8cfa
ollie <ydsn336@naver.com>
2022-04-28 16:41:13 +0900

    postController생성및 post폴더 생성

---
+++ src/main/java/com/example/demo/src/post/PostController.java
@@ -0,0 +1,40 @@
+package com.example.demo.src.post;
+
+import com.example.demo.src.user.UserProvider;
+import com.example.demo.src.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.user.model.*;
+import com.example.demo.utils.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.example.demo.config.BaseResponseStatus.*;
+import static com.example.demo.config.BaseResponseStatus.POST_USERS_ANOTHER_PASSWORD;
+import static com.example.demo.utils.ValidationRegex.isRegexEmail;
+import static com.example.demo.utils.ValidationRegex.isRegexPassword;
+
+@RestController
+@RequestMapping("/app/posts")
+public class PostController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final PostProvider postProvider;
+    @Autowired
+    private final PostService postService;
+    @Autowired
+    private final JwtService jwtService;
+
+
+    public PostController(PostProvider postProvider, PostService postService, JwtService jwtService){
+        this.postProvider = postProvider;
+        this.postService = postService;
+        this.jwtService = jwtService;
+    }
+    
+}
---
+++ src/main/java/com/example/demo/src/post/PostProvider.java
@@ -0,0 +1,32 @@
+package com.example.demo.src.post;
+import com.example.demo.config.BaseException;
+import com.example.demo.config.secret.Secret;
+import com.example.demo.src.user.UserDao;
+import com.example.demo.src.user.model.*;
+import com.example.demo.utils.AES128;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.example.demo.config.BaseResponseStatus.*;
+
+//Provider : Read의 비즈니스 로직 처리
+@Service
+public class PostProvider {
+
+    private final PostDao postDao;
+    private final JwtService jwtService;
+
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    public PostProvider(PostDao postDao, JwtService jwtService) {
+        this.postDao = postDao;
+        this.jwtService = jwtService;
+    }
+}
