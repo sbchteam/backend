33db64b9b8e76994be257b59b65016ac3bfe1ad4
Gahyun Lee <ab3380@naver.com>
2022-05-24 22:00:21 +0900

    댓글 조회 리스트 추가

---
+++ src/main/java/com/example/demo/src/post/model/PostCommentList.java
@@ -0,0 +1,17 @@
+package com.example.demo.src.post.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostCommentList {
+
+    private int userId;
+    private int parentId;
+    private String nick;
+    private String comment;
+    private String createdAt;
+}
