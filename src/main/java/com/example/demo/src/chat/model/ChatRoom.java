8cc3c705416b589ae7152517e77f9881e9321303
ollie <ydsn336@naver.com>
2022-07-12 16:44:45 +0900

    채팅코드추가

---
+++ src/main/java/com/example/demo/src/chat/model/ChatRoom.java
@@ -0,0 +1,25 @@
+package com.example.demo.src.chat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoom {
+    private String roomId;
+    private String roomName;
+
+
+    public static ChatRoom create(String name) {
+        ChatRoom room = new ChatRoom();
+        room.roomId = UUID.randomUUID().toString();
+        room.roomName = name;
+        return room;
+    }
+}
