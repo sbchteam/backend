f5b732314d02d7b6500679d5505b473d67c746d9
ollie <ydsn336@naver.com>
2022-07-23 15:59:32 +0900

    message controller update

--- src/main/java/com/example/demo/src/chat/MessageController.java
+++ src/main/java/com/example/demo/src/chat/MessageController.java
@@ -16,28 +16,28 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
-//    @Autowired
-//    private final ChatService chatService;
-//    @Autowired
-//    private final JwtService jwtService;
-//
-//
-//    private final SimpMessageSendingOperations sendingOperations;
-//
-//    @MessageMapping("/chat/message")
-//    public void enter(ChatMessage message) {
-//
-//        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-//            message.setMessage(message.getSender()+"님이 입장하였습니다.");
-//            chatService.enterRoom(message.getRoomId(),message.getSender());
-//        }else{
-//            //메시지 저장
-//            chatService.createMessage(message);
-//        }
-//        sendingOperations.convertAndSend("/sub/chat/room/"+message.getRoomId(),message);
-//
-//
-//
-//
-//    }
+    @Autowired
+    private final ChatService chatService;
+    @Autowired
+    private final JwtService jwtService;
+
+
+    private final SimpMessageSendingOperations sendingOperations;
+
+    @MessageMapping("/chat/message")
+    public void enter(ChatMessage message) {
+
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getSender()+"님이 입장하였습니다.");
+            chatService.enterRoom(message.getRoomId(),message.getSender());
+        }else{
+            //메시지 저장
+            chatService.createMessage(message);
+        }
+        sendingOperations.convertAndSend("/sub/chat/room/"+message.getRoomId(),message);
+
+
+
+
+    }
 }
