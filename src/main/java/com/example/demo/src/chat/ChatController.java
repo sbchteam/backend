3d9d11ea28a90f004a13b27fe8fa5ef0d2f3ce34
ollie <ydsn336@naver.com>
2022-07-23 15:23:44 +0900

    필요없는 파일 삭제

--- src/main/java/com/example/demo/src/chat/ChatController.java
+++ src/main/java/com/example/demo/src/chat/ChatController.java
@@ -89,15 +89,4 @@ public class ChatController {
     }
 
 
-    // 채팅 리스트 화면
-    @GetMapping("/room")
-    public String rooms(Model model) {
-        return "chat/room";
-    }
-    // 채팅방 입장 화면
-    @GetMapping("/room/enter/{roomId}")
-    public String roomDetail(Model model, @PathVariable String roomId) {
-        model.addAttribute("roomId", roomId);
-        return "chat/roomdetail";
-    }
 }
